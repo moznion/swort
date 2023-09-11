ff3b148d7fa549a6a4b4d9401b8bbf23f3bce70b
moznion <moznion@mail.moznion.net>
2023-09-11 16:27:37 -0700

    Implement fundamental functions
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ slice.go
@@ -0,0 +1,140 @@
+package swort
+
+import (
+	"cmp"
+	"slices"
+)
+
+type Slice[IT any, ST cmp.Ordered] struct {
+	sortValues            []ST
+	sortValue2Item        map[ST][]IT
+	items                 []IT
+	originalSortValue2Idx map[ST]int
+	ascSortedItems        []IT
+	descSortValue2Idx     map[ST]int
+	descSortedItems       []IT
+	ascSortValue2Idx      map[ST]int
+	sortValueExtractor    func(item IT) ST
+}
+
+func MakeSlice[IT any, ST cmp.Ordered](items []IT, sortValueExtractor func(item IT) ST) *Slice[IT, ST] {
+	sortValues := make([]ST, 0, len(items))
+	sortValue2Item := map[ST][]IT{}
+	originalSortValue2Idx := map[ST]int{}
+
+	for i, item := range items {
+		sortVal := sortValueExtractor(item)
+
+		gotItems, ok := sortValue2Item[sortVal]
+		if !ok {
+			sortValues = append(sortValues, sortVal)
+			sortValue2Item[sortVal] = []IT{item}
+			originalSortValue2Idx[sortVal] = i
+			continue
+		}
+
+		sortValue2Item[sortVal] = append(gotItems, item)
+	}
+
+	return &Slice[IT, ST]{
+		sortValues:            sortValues,
+		sortValue2Item:        sortValue2Item,
+		originalSortValue2Idx: originalSortValue2Idx,
+		items:                 items,
+		sortValueExtractor:    sortValueExtractor,
+	}
+}
+
+func (s *Slice[IT, ST]) Len() int {
+	return len(s.items)
+}
+
+func (s *Slice[IT, ST]) SearchFromOriginal(item IT) (int, bool) {
+	idx, ok := s.originalSortValue2Idx[s.sortValueExtractor(item)]
+	if !ok {
+		idx = s.Len()
+	}
+	return idx, ok
+
+}
+
+func (s *Slice[IT, ST]) SearchFromSortedByAsc(item IT) (int, bool) {
+	if s.ascSortValue2Idx == nil {
+		s.sortByAsc()
+	}
+
+	idx, ok := s.ascSortValue2Idx[s.sortValueExtractor(item)]
+	if !ok {
+		idx = s.Len()
+	}
+	return idx, ok
+}
+
+func (s *Slice[IT, ST]) SearchFromSortedByDesc(item IT) (int, bool) {
+	if s.descSortValue2Idx == nil {
+		s.sortByDesc()
+	}
+
+	idx, ok := s.descSortValue2Idx[s.sortValueExtractor(item)]
+	if !ok {
+		idx = s.Len()
+	}
+	return idx, ok
+}
+
+func (s *Slice[IT, ST]) SortByAsc() []IT {
+	return s.sortByAsc()
+}
+
+func (s *Slice[IT, ST]) SortByDesc() []IT {
+	return s.sortByDesc()
+}
+
+func (s *Slice[IT, ST]) sortByAsc() []IT {
+	if ascSorted := s.ascSortedItems; ascSorted != nil {
+		return ascSorted
+	}
+
+	slices.Sort(s.sortValues)
+
+	s.ascSortValue2Idx = make(map[ST]int)
+	s.ascSortedItems = make([]IT, 0, len(s.items))
+	idx := 0
+	for _, sortValue := range s.sortValues {
+		for _, item := range s.sortValue2Item[sortValue] {
+			s.ascSortedItems = append(s.ascSortedItems, item)
+
+			if _, ok := s.ascSortValue2Idx[sortValue]; !ok {
+				s.ascSortValue2Idx[sortValue] = idx
+			}
+			idx++
+		}
+	}
+
+	return s.ascSortedItems
+}
+
+func (s *Slice[IT, ST]) sortByDesc() []IT {
+	if descSorted := s.descSortedItems; descSorted != nil {
+		return descSorted
+	}
+
+	ascSorted := s.sortByAsc()
+	l := len(ascSorted)
+
+	s.descSortValue2Idx = make(map[ST]int)
+	s.descSortedItems = make([]IT, l)
+	idx := 0
+	for i := l - 1; i >= 0; i-- {
+		s.descSortedItems[idx] = ascSorted[i]
+
+		sortValue := s.sortValueExtractor(ascSorted[i])
+		if _, ok := s.descSortValue2Idx[sortValue]; !ok {
+			s.descSortValue2Idx[sortValue] = idx
+		}
+
+		idx++
+	}
+
+	return s.descSortedItems
+}
---
+++ slice_test.go
@@ -0,0 +1,210 @@
+package swort
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Struct struct {
+	intValue int64
+}
+
+func TestSlice_Len(t *testing.T) {
+	given := []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5} /* <= duplicated */, {intValue: 4}}
+	slice := MakeSlice(given, func(s Struct) int64 {
+		return s.intValue
+	})
+
+	assert.Equal(t, 7, slice.Len())
+}
+
+func TestSlice_SortByAsc(t *testing.T) {
+	given := []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5} /* <= duplicated */, {intValue: 4}}
+	slice := MakeSlice(given, func(s Struct) int64 {
+		return s.intValue
+	})
+
+	assert.EqualValues(t, []Struct{{intValue: 1}, {intValue: 2}, {intValue: 3}, {intValue: 4}, {intValue: 5}, {intValue: 5}, {intValue: 6}}, slice.SortByAsc(), "should be sorted by ASC")
+	assert.EqualValues(t, []Struct{{intValue: 1}, {intValue: 2}, {intValue: 3}, {intValue: 4}, {intValue: 5}, {intValue: 5}, {intValue: 6}}, slice.SortByAsc(), "should be sorted by ASC; check for retrieval from cache")
+	assert.EqualValues(t, []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5}, {intValue: 4}}, given, "should given be immutable")
+}
+
+func TestSlice_SortByDesc(t *testing.T) {
+	given := []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5} /* <= duplicated */, {intValue: 4}}
+	slice := MakeSlice(given, func(s Struct) int64 {
+		return s.intValue
+	})
+
+	assert.EqualValues(t, []Struct{{intValue: 6}, {intValue: 5}, {intValue: 5}, {intValue: 4}, {intValue: 3}, {intValue: 2}, {intValue: 1}}, slice.SortByDesc(), "should be sorted by Desc")
+	assert.EqualValues(t, []Struct{{intValue: 6}, {intValue: 5}, {intValue: 5}, {intValue: 4}, {intValue: 3}, {intValue: 2}, {intValue: 1}}, slice.SortByDesc(), "should be sorted by Desc, check for retrieval from cache")
+	assert.EqualValues(t, []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5}, {intValue: 4}}, given, "should given be immutable")
+}
+
+func TestSlice_SearchFromOriginal(t *testing.T) {
+	given := []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5} /* <= duplicated */, {intValue: 4}}
+	slice := MakeSlice(given, func(s Struct) int64 {
+		return s.intValue
+	})
+
+	type testCase struct {
+		intValue    int64
+		expectedIdx int
+		expectedOK  bool
+	}
+	testCases := []testCase{
+		{
+			intValue:    5,
+			expectedIdx: 0,
+			expectedOK:  true,
+		},
+		{
+			intValue:    2,
+			expectedIdx: 1,
+			expectedOK:  true,
+		},
+		{
+			intValue:    6,
+			expectedIdx: 2,
+			expectedOK:  true,
+		},
+		{
+			intValue:    3,
+			expectedIdx: 3,
+			expectedOK:  true,
+		},
+		{
+			intValue:    1,
+			expectedIdx: 4,
+			expectedOK:  true,
+		},
+		{
+			intValue:    4,
+			expectedIdx: 6,
+			expectedOK:  true,
+		},
+		{
+			intValue:    7,
+			expectedIdx: 7,
+			expectedOK:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got, ok := slice.SearchFromOriginal(Struct{intValue: tc.intValue})
+		assert.Equal(t, got, tc.expectedIdx)
+		assert.Equal(t, ok, tc.expectedOK)
+	}
+}
+
+func TestSlice_SearchFromSortedByAsc(t *testing.T) {
+	given := []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5} /* <= duplicated */, {intValue: 4}}
+	slice := MakeSlice(given, func(s Struct) int64 {
+		return s.intValue
+	})
+
+	type testCase struct {
+		intValue    int64
+		expectedIdx int
+		expectedOK  bool
+	}
+	testCases := []testCase{
+		{
+			intValue:    1,
+			expectedIdx: 0,
+			expectedOK:  true,
+		},
+		{
+			intValue:    2,
+			expectedIdx: 1,
+			expectedOK:  true,
+		},
+		{
+			intValue:    3,
+			expectedIdx: 2,
+			expectedOK:  true,
+		},
+		{
+			intValue:    4,
+			expectedIdx: 3,
+			expectedOK:  true,
+		},
+		{
+			intValue:    5,
+			expectedIdx: 4,
+			expectedOK:  true,
+		},
+		{
+			intValue:    6,
+			expectedIdx: 6,
+			expectedOK:  true,
+		},
+		{
+			intValue:    7,
+			expectedIdx: 7,
+			expectedOK:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got, ok := slice.SearchFromSortedByAsc(Struct{intValue: tc.intValue})
+		assert.Equal(t, got, tc.expectedIdx)
+		assert.Equal(t, ok, tc.expectedOK)
+	}
+}
+
+func TestSlice_SearchFromSortedByDesc(t *testing.T) {
+	given := []Struct{{intValue: 5}, {intValue: 2}, {intValue: 6}, {intValue: 3}, {intValue: 1}, {intValue: 5} /* <= duplicated */, {intValue: 4}}
+	slice := MakeSlice(given, func(s Struct) int64 {
+		return s.intValue
+	})
+
+	type testCase struct {
+		intValue    int64
+		expectedIdx int
+		expectedOK  bool
+	}
+	testCases := []testCase{
+		{
+			intValue:    6,
+			expectedIdx: 0,
+			expectedOK:  true,
+		},
+		{
+			intValue:    5,
+			expectedIdx: 1,
+			expectedOK:  true,
+		},
+		{
+			intValue:    4,
+			expectedIdx: 3,
+			expectedOK:  true,
+		},
+		{
+			intValue:    3,
+			expectedIdx: 4,
+			expectedOK:  true,
+		},
+		{
+			intValue:    2,
+			expectedIdx: 5,
+			expectedOK:  true,
+		},
+		{
+			intValue:    1,
+			expectedIdx: 6,
+			expectedOK:  true,
+		},
+		{
+			intValue:    7,
+			expectedIdx: 7,
+			expectedOK:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got, ok := slice.SearchFromSortedByDesc(Struct{intValue: tc.intValue})
+		assert.Equal(t, got, tc.expectedIdx)
+		assert.Equal(t, ok, tc.expectedOK)
+	}
+}
